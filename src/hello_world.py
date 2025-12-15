a41da902ce36404252481e2be6152ead422121a5
Henil Patel <henil6984@gmail.com>
2025-12-15 23:47:03 +0530

    Update greeting message in hello_world.py
    Replaced 'Hello, World!' with 'Hello From GitHub'.

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,7 +1,7 @@
 class HelloWorld:
     @staticmethod
     def main():
-        print("Hello, World!")
-
+       # print("Hello, World!")
+        print ("Hello From GitHub")
 if __name__ == "__main__":
     HelloWorld.main()
